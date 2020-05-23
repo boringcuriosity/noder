f1c0fab73f96da5b400715a26c4da7e4e4815c18
Shaheer Ahmed <62714282+boringcuriosity@users.noreply.github.com>
2020-05-23 13:12:34 +0530

    Update _siteConfig.js

--- config/_siteConfig.js
+++ config/_siteConfig.js
@@ -74,7 +74,7 @@ export default {
     mailchimp: {
       on: true,
       formAction:
-        'https://danielkelly.us3.list-manage.com/subscribe/post?u=d3c2e762b66a2353d0bc29bf3&amp;id=2abeaa06e0'
+        'https://tech.us4.list-manage.com/subscribe/post?u=b7f1f1fabdcb0ea3db6d7bd95&amp;id=f540d4b02e'
     },
     custom: {
       on: false,
